15922c1c501f18bb086b5c0f35d9454ea441ac6c
Kuei-Jung Hu <kueijunghu@gmail.com>
2024-11-07 21:54:08 +0900

    Implement the splash screen

--- composeApp/src/androidMain/kotlin/io/github/akueisara/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/io/github/akueisara/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.github.akueisara.ui.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         setContent {
