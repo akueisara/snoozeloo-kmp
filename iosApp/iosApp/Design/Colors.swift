01c7beddf35feb1b90efa5692d7af10e1830d2bd
Kuei-Jung Hu <kueijunghu@gmail.com>
2024-11-19 00:44:21 +0900

    Add color

---
+++ iosApp/iosApp/Design/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  iosApp
+//
+//  Created by Kuei Jung Hu on 2024/11/19.
+//  Copyright © 2024 Kuei Jung Hu. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primary: Color = Color("Primary")
+    static let onSurface: Color = Color("onSurface")
+    static let secondary: Color = Color("Secondary")
+}
